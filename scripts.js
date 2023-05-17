7a66e5a4d6bd52d575fc4d4fa88ede6f873f96bb
Babak <hadadybabak@gmail.com>
2023-05-17 02:10:21 -0700

    Added about page support, fixed add item asynchronous call

--- scripts.js
+++ scripts.js
@@ -2,11 +2,28 @@
 let initial_items = '[{"name": "Bike", "description": "a nice bike", "price": "100", "url": "https://cdn.shopify.com/s/files/1/0541/0154/1047/products/0711964_b_1200x1200.jpg?v=1614971567"},	{"name": "Candy", "description": "a delicious treat", "price": "3", "url": "https://assets.shop.loblaws.ca/products/21210265/b1/en/front/21210265_front_a01_@2.png" }]';
 let items = [];
 
+let home;
+
+
+
+changePage(true);
+
 
 items = items.concat(JSON.parse(initial_items));
 initializeCards();
 
-function initializeCards(){
+function changePage(home) {
+	if (home) {
+		document.getElementById("home").style.display ="inherit";
+		document.getElementById("about").style.display ="none";	
+
+	} else {
+		document.getElementById("home").style.display = "none";
+		document.getElementById("about").style.display ="inherit";	
+	}
+}
+
+async function initializeCards(){
 	let card_group = document.getElementsByClassName("card-group").item(0);
 	for (let i = 0; i < items.length; i++) {
 		let new_item = setupCard(items[i]);
@@ -15,11 +32,12 @@ function initializeCards(){
 }
 
 
-function submitCard() {
+async function submitCard() {
 	let card_group = document.getElementsByClassName("card-group").item(0);
 	card_group.innerHTML = "";
-	addItem();
+	addItem().then(() => {
 	initializeCards();
+	});
 }
 
 
@@ -28,25 +46,28 @@ function deleteCards() {
 	items = [];
 }
 
-function addItem() {
-
+async function addItem() {
 	let form = document.getElementById("card-form");
 	let name = form.name.value;
 	let price = form.price.value;
 	let description = form.desc.value;
 	let url = form.url.value;
-	items.push({
-		"name": name,
-		"price": price,
-		"description": description,
-		"url": url,
-	});
+	if (!name || !price || !description || !url) return;
+
+	await checkURL(url).then((res) => {
+		if(!res) return;
+		items.push({
+			"name": name,
+			"price": price,
+			"description": description,
+			"url": url,
+		});
+	})
 }
 
 function setupCard(item) {
 	let card = document.createElement("li");
-	card.classList.add("card-container")
-	
+	card.classList.add("card-container");
 
 	for (const prop in item) {
 		let element;
@@ -57,10 +78,24 @@ function setupCard(item) {
 		} else {
 			element = document.createElement("p");
 			element.classList.add("card-" + prop);
-			element.appendChild(document.createTextNode(item[prop]));
+			if (prop === "price") {
+				element.appendChild(document.createTextNode("Cost: " + item[prop]));
+			} else {
+				element.appendChild(document.createTextNode(item[prop]));
+			}
 		}
 		card.appendChild(element);
 	}
 
 	return card;
 }
+
+
+async function checkURL(url) {
+	return fetch(url).then((res)=> {
+		return res.ok;
+	}).catch((e) => {
+		return false;
+	});
+}
+
