44966bd0b3fd82df075ad560d7aff9f1379b303d
Babak <hadadybabak@gmail.com>
2023-05-21 20:21:27 -0700

    added card deletion support and hover transitions

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-
-let initial_items = '[{"name": "Bike", "description": "a nice bike", "price": "100", "url": "https://cdn.shopify.com/s/files/1/0541/0154/1047/products/0711964_b_1200x1200.jpg?v=1614971567"},	{"name": "Candy", "description": "a delicious treat", "price": "3", "url": "https://assets.shop.loblaws.ca/products/21210265/b1/en/front/21210265_front_a01_@2.png" }]';
+changePage(true);
+let initial_items = '[{"name": "Bike", "description": "a nice bike", "price": "100", "url": "https://cdn.shopify.com/s/files/1/0541/0154/1047/products/0711964_b_1200x1200.jpg?v=1614971567"}, {"name": "Candy", "description": "a delicious treat", "price": "3", "url": "https://assets.shop.loblaws.ca/products/21210265/b1/en/front/21210265_front_a01_@2.png"}, {"name": "Dog", "description": "a good boy", "price": "10", "url": "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g"}]';
 let items = [];
 
 let home;
 
 
 
-changePage(true);
 
 
 items = items.concat(JSON.parse(initial_items));
@@ -25,8 +24,9 @@ function changePage(home) {
 
 async function initializeCards(){
 	let card_group = document.getElementsByClassName("card-group").item(0);
+	card_group
 	for (let i = 0; i < items.length; i++) {
-		let new_item = setupCard(items[i]);
+		let new_item = setupCard(items[i], i);
 		card_group.appendChild(new_item);
 	}
 }
@@ -36,13 +36,20 @@ async function submitCard() {
 	let card_group = document.getElementsByClassName("card-group").item(0);
 	card_group.innerHTML = "";
 	addItem().then(() => {
-	initializeCards();
+		initializeCards();
 	});
 }
 
+function deleteChildren() {
+	element = document.getElementsByClassName("card-group").item(0);
+	
+	while(element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+}
 
 function deleteCards() {
-	document.getElementsByClassName("card-group").item(0).innerHTML = "";
+	deleteChildren();
 	items = [];
 }
 
@@ -65,7 +72,7 @@ async function addItem() {
 	})
 }
 
-function setupCard(item) {
+function setupCard(item, i) {
 	let card = document.createElement("li");
 	card.classList.add("card-container");
 
@@ -79,15 +86,19 @@ function setupCard(item) {
 			element = document.createElement("p");
 			element.classList.add("card-" + prop);
 			if (prop === "price") {
-				element.appendChild(document.createTextNode("Cost: " + item[prop]));
+				element.appendChild(document.createTextNode("$" + item[prop]));
 			} else {
 				element.appendChild(document.createTextNode(item[prop]));
 			}
 		}
 		card.appendChild(element);
 	}
-
-	return card;
+		card.id = i;
+		card.onclick = () => {
+			deleteCard(card);
+		}
+	
+		return card;
 }
 
 
@@ -99,3 +110,9 @@ async function checkURL(url) {
 	});
 }
 
+async function deleteCard(item) {
+	console.log(item.id);
+	items.splice(item.id, 1);
+	deleteChildren();
+	initializeCards();	
+}
