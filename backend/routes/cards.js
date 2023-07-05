9c1dca9584407c054e6829616ccad7543500f2bb
Babak <hadadybabak@gmail.com>
2023-07-04 23:18:30 -0700

    finished assignment4

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,30 +3,6 @@ var router = express.Router();
 
 const Card = require("../model");
 
-let cards = [
-  {
-    name: "Bike",
-    description: "a nice bike",
-    price: "100",
-    count: 1,
-    url: "https://cdn.shopify.com/s/files/1/0541/0154/1047/products/0711964_b_1200x1200.jpg?v=1614971567",
-  },
-  {
-    name: "Candy",
-    description: "a delicious treat",
-    price: "3",
-    count: 1,
-    url: "https://assets.shop.loblaws.ca/products/21210265/b1/en/front/21210265_front_a01_@2.png",
-  },
-  {
-    name: "Dog",
-    description: "a good boy",
-    price: "10",
-    count: 1,
-    url: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g",
-  },
-];
-
 /* GET cards. */
 router.get("/", async function (req, res) {
   const cards = await Card.find({});
@@ -71,6 +47,23 @@ router.patch("/remove/:card", async function (req, res) {
   const card = await Card.findOne({ name: req.params.card });
   res.status(200);
   res.send(card);
+});
+
+/* GET total count */
+router.get("/count/all", async function (req, res) {
+  console.log("RAN");
+  let count = await Card.aggregate([
+    {
+      $group: {
+        _id: null,
+        count: { $sum: "$count" },
+      },
+    },
+  ]);
+
+  console.log(count);
+  res.status(200);
+  res.send(count);
 });
 
 function makeSKU() {
